a110f4aed49a1947d14788b5f518be96877eeb8e
長田 凌 <nagata@private.wide.co.jp>
2019-05-16 15:54:05 +0900

    画面表示できない

--- index.php
+++ index.php
@@ -16,21 +16,21 @@ $errorMessage = "";
 if(isset($_POST['login'])){
 	if(empty($_POST['name'])){
 		$errorMessage = '名前を入力してください';
-		exit();
+		
+	}else{
+		//DB接続情報作成
+		$connectString = "host={$db['host']} dbname={$db['dbname']} port=5432 user={$db['user']} password={$db['pass']}";
+		//DB接続
+		if(!$result = pg_connect($connectString)){
+			//接続失敗
+			$errorMessage = '予期せぬエラーが発生';
+			exit();
+		}
+		$select = sprintf("SELECT * FROM questionnaire WHERE name = %s",$_POST['name']);
+		$selectresult = pg_query($select);
+		$array = pg_fetch_array($selectresult,0,PGSQL_NUM);
 	}
-	
-	//DB接続情報作成
-	$connectString = "host={$db['host']} dbname={$db['dbname']} port=5432 user={$db['user']} password={$db['pass']}";
-	//DB接続
-	if(!$result = pg_connect($connectString)){
-		//接続失敗
-		$errorMessage = '予期せぬエラーが発生';
-		exit();
-	}
-	$select = sprintf("SELECT * FROM questionnaire WHERE name = %s",$_POST['name']);
-	$selectresult = pg_query($select);
-	$array = pg_fetch_array($selectresult,0,PGSQL_NUM);
-	
+		
 	//入力した名前とDBにある名前が一致した場合
 	if($_POST['name'] = $array[1]){
 		$errorMesage = '入力された名前は既に使われています。';
@@ -38,9 +38,10 @@ if(isset($_POST['login'])){
 	$_SESSION['name'] = $_POST['name'];
 
 	pg_close($result);
-	header("Location:questionnaire.php");
+	header("Location: questionnaire.php");
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
